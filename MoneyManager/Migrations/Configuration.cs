005edfbfbc06c2ffd264930f5c2792d4e4330b74
AliveDevil <alivedevil@alivedevil.de>
2015-02-14 13:03:14 +0100

    Layouting and seeding.

--- MoneyManager/Migrations/Configuration.cs
+++ MoneyManager/Migrations/Configuration.cs
@@ -1,6 +1,6 @@
 using MoneyManager.Model;
 using System.Data.Entity.Migrations;
-
+using System.Linq;
 namespace MoneyManager.Migrations
 {
 	internal sealed class Configuration : DbMigrationsConfiguration<DatabaseContext>
@@ -13,6 +13,10 @@ namespace MoneyManager.Migrations
 
 		protected override void Seed(DatabaseContext context)
 		{
+			if (!context.AccountSet.Any())
+			{
+				context.AccountSet.Add(new Account() { Name = "Default" });
+			}
 			context.TagSet.AddOrUpdate(p => p.Key, new Tag() { Key = "Default", Default = true });
 		}
 	}
