d0d9f581d9f8541d9c45f20929e03dac557d7150
Jöran Malek <AliveDevil@users.noreply.github.com>
2015-06-28 17:07:26 +0200

    Reset to .NET 4.5 Added simple about-text.

--- MoneyManager/Views/AboutView.xaml.cs
+++ MoneyManager/Views/AboutView.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,11 @@ namespace MoneyManager.Views
 		public AboutView()
 		{
 			InitializeComponent();
+		}
+
+		private void Hyperlink_Click(object sender, RoutedEventArgs e)
+		{
+			Process.Start(((Hyperlink)sender).NavigateUri.ToString());
 		}
 	}
 }
