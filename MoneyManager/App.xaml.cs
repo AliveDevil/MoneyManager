932787446a81074aa407ad43741fce6cb98dbfb9
Jöran Malek <AliveDevil@users.noreply.github.com>
2015-07-14 08:17:26 +0200

    Update-things. Will not work, because nUpdate uses strongnamed Newtonsoft.Json 6.0.0.0.

--- MoneyManager/App.xaml.cs
+++ MoneyManager/App.xaml.cs
@@ -33,7 +33,7 @@ namespace MoneyManager
 		public event PropertyChangedEventHandler PropertyChanged;
 
 		private FileInfo settingsFile;
-		private UpdateManager updater;
+		public UpdateManager Updater { get; private set; }
 		private bool updatesAvailable;
 
 		public Settings AppSettings { get; private set; }
@@ -84,23 +84,31 @@ namespace MoneyManager
 				Settings.Save(AppSettings, settingsFile);
 			}
 
-			updater = new UpdateManager(new Uri("http://dl.alivedevil.de/update/moneymanager/updates.json"), "<RSAKeyValue><Modulus>3a88jyS9mfHONnEOu8KVtOCcIUYC8rJGT88HIhr/tBWHc0V3oeuKPqnYA7RB097HlcrywKt8mm4/LTD3endINhlC5hhpqAMgGeajImmm7OKTfHNnOVd6iQKpEBq1yr9CK5XnBrviyW3kZiSVIIid0+U0ubSXvEIaNfg6LouUI/ndNtNwHfP0zAKyr5m7IXIJYitnhtH4c63fjVLFsdO8K5WiFRoJ29npfCrpc2rdm1uwP/xnfmT5O4GwYQeJm32PZ6zX78GIQ6MaadS88lwai1L+NuFapAx7aHy4P+2CsLeysBmmpAxbyNxmzqXG/m4hF8tN7lwdcmY9zI6HPmIhOL8H3ISTbHOrlIZIM58alo704WT68p9vWzTKKscHFHj7IeJHR9VEVauZ2BGZxpnwxVbmkVwT9aHt+v1ZxvuHtReW0sgP815dN8Dwb5jZO7esYirkt89k9jOAjh+ZRjnjuVoEpCKXChKKLhHjZmAD6e9kz5E0igwzJ/HQjPqXNhFf9H5NfgJY95FFL3cBjH9ON27auC2l4Pb5mhRl2IhnAciRWcOhICkDq44O/6G+01PWQC9fazRFCwNvz6PSm2zLKLPoAyPmH9vz1Q+pCeIYk2nRvRDIe8pUMMSeheHZuK4g1QIIQmuD47VZdZNBVPDafe7jQ4XHpnk4gNiLYKonRYvgYAIWMnFeJwSpX1uPf1zxzRNhFsx6FUFC2AWw/3mcJJUlp0NYGaIolgj9ednDen7ol8oeaFi8uajzXFBcPBlTekM89ACoMqzv7G915DWzimL8V7keIab75Mwc1MvqP0lgXD73YbwtXLykq2uZGpyk8L/10xdwk0RHftxn8g/k5GAO7aEJvO6MSDvKZ0GUeUyIN6nT4uOuFegIUPE+XLKGoUOkE3t3EWQKs7IlVUByiURuqz+llZh2bkQy/XHaybdJ6HiArckXGAJxDIlu27xld/luGHWduIdyPPvIx+QgBt93v2N1ASkPSlNrmtV7HO/1OvQX4dOFiPMUDJgOiecFUMlSeaejOoeJcsKrZV9zuz6K1PBXNDLL+YbF+0ER57SS8fT1w2uHwx1bVC/9pF6B4EXNcv7zNNNAs0x0iGsSoPTV4lUK+E0xdujxEKjYBpPiLM3ISyYfheD0A3tC4yZEjKaXNfk4rb59IFg2hDYoURsoYJnvJgsEu6kRI7SDlVW+Lmh++BICprLf3K3OdXrmEavjqvcp3xrSbDgKj3ubzHdWLS2JvuCcQMdvFlmqQn0yW+skq/AfgefviVQ7Wy0tLrN8jNCEGlOh0ujgxliWmex5zVschjvfNbfp5+lOxi4SbnUV9iNG5wi7qtxbTdIXBiknBdMnZ50KSf/4Ymc9UQ==</Modulus><Exponent>AQAB</Exponent></RSAKeyValue>", new CultureInfo("en"));
-			updater.CloseHostApplication = true;
-			updater.UseCustomInstallerUserInterface = true;
-			updater.IncludeBeta = AppSettings.CheckBetaVersions;
-
-			CheckForUpdates();
+			Updater = new UpdateManager(new Uri("http://dl.alivedevil.de/update/moneymanager/updates.json"), "<RSAKeyValue><Modulus>3a88jyS9mfHONnEOu8KVtOCcIUYC8rJGT88HIhr/tBWHc0V3oeuKPqnYA7RB097HlcrywKt8mm4/LTD3endINhlC5hhpqAMgGeajImmm7OKTfHNnOVd6iQKpEBq1yr9CK5XnBrviyW3kZiSVIIid0+U0ubSXvEIaNfg6LouUI/ndNtNwHfP0zAKyr5m7IXIJYitnhtH4c63fjVLFsdO8K5WiFRoJ29npfCrpc2rdm1uwP/xnfmT5O4GwYQeJm32PZ6zX78GIQ6MaadS88lwai1L+NuFapAx7aHy4P+2CsLeysBmmpAxbyNxmzqXG/m4hF8tN7lwdcmY9zI6HPmIhOL8H3ISTbHOrlIZIM58alo704WT68p9vWzTKKscHFHj7IeJHR9VEVauZ2BGZxpnwxVbmkVwT9aHt+v1ZxvuHtReW0sgP815dN8Dwb5jZO7esYirkt89k9jOAjh+ZRjnjuVoEpCKXChKKLhHjZmAD6e9kz5E0igwzJ/HQjPqXNhFf9H5NfgJY95FFL3cBjH9ON27auC2l4Pb5mhRl2IhnAciRWcOhICkDq44O/6G+01PWQC9fazRFCwNvz6PSm2zLKLPoAyPmH9vz1Q+pCeIYk2nRvRDIe8pUMMSeheHZuK4g1QIIQmuD47VZdZNBVPDafe7jQ4XHpnk4gNiLYKonRYvgYAIWMnFeJwSpX1uPf1zxzRNhFsx6FUFC2AWw/3mcJJUlp0NYGaIolgj9ednDen7ol8oeaFi8uajzXFBcPBlTekM89ACoMqzv7G915DWzimL8V7keIab75Mwc1MvqP0lgXD73YbwtXLykq2uZGpyk8L/10xdwk0RHftxn8g/k5GAO7aEJvO6MSDvKZ0GUeUyIN6nT4uOuFegIUPE+XLKGoUOkE3t3EWQKs7IlVUByiURuqz+llZh2bkQy/XHaybdJ6HiArckXGAJxDIlu27xld/luGHWduIdyPPvIx+QgBt93v2N1ASkPSlNrmtV7HO/1OvQX4dOFiPMUDJgOiecFUMlSeaejOoeJcsKrZV9zuz6K1PBXNDLL+YbF+0ER57SS8fT1w2uHwx1bVC/9pF6B4EXNcv7zNNNAs0x0iGsSoPTV4lUK+E0xdujxEKjYBpPiLM3ISyYfheD0A3tC4yZEjKaXNfk4rb59IFg2hDYoURsoYJnvJgsEu6kRI7SDlVW+Lmh++BICprLf3K3OdXrmEavjqvcp3xrSbDgKj3ubzHdWLS2JvuCcQMdvFlmqQn0yW+skq/AfgefviVQ7Wy0tLrN8jNCEGlOh0ujgxliWmex5zVschjvfNbfp5+lOxi4SbnUV9iNG5wi7qtxbTdIXBiknBdMnZ50KSf/4Ymc9UQ==</Modulus><Exponent>AQAB</Exponent></RSAKeyValue>", new CultureInfo("en"));
+			Updater.CloseHostApplication = true;
+			Updater.UseCustomInstallerUserInterface = true;
+			Updater.IncludeBeta = AppSettings.CheckBetaVersions;
 
 			ViewState.Set<LoadDatabaseViewModel>();
+
+			CheckForUpdates();
 
 			base.OnStartup(e);
 		}
 
 		private async Task CheckForUpdates()
 		{
-			if (await updater.SearchForUpdatesAsync())
+			try
 			{
-				updatesAvailable = true;
+				if (await Updater.SearchForUpdatesAsync())
+				{
+					updatesAvailable = true;
+					ViewState.Push<UpdaterViewModel>();
+				}
+			}
+			catch (Exception e)
+			{
+				;
 			}
 		}
 	}
---
+++ MoneyManager/ViewModel/UpdaterViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System.Diagnostics;
+
+namespace MoneyManager.ViewModel
+{
+	public class UpdaterViewModel : ViewModelBase
+	{
+		private RelayCommand backCommand;
+		private RelayCommand aboutCommand;
+		private RelayCommand donateCommand;
+
+		public RelayCommand BackCommand
+		{
+			get
+			{
+				return backCommand ?? (backCommand = new RelayCommand(() =>
+				{
+					App.ViewState.Pop();
+				}));
+			}
+		}
+
+		public RelayCommand AboutCommand
+		{
+			get
+			{
+				return aboutCommand ?? (aboutCommand = new RelayCommand(() =>
+				{
+					App.ViewState.Push<AboutViewModel>();
+				}));
+			}
+		}
+
+		public RelayCommand DonateCommand
+		{
+			get
+			{
+				return donateCommand ?? (donateCommand = new RelayCommand(() =>
+				{
+					Process.Start("http://donation-tracker.com/u/alivedevil");
+				}));
+			}
+		}
+	}
+}
